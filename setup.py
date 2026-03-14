b4cb01c7d0556f03b33687f8dde9e4a9601b706d
Shatadru281 <shatadruadhikary05@gmail.com>
2026-03-15 02:46:32 +0530

    web app completed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Shatadru Adhikary",
+    author_email="shatadruadhikary05@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
